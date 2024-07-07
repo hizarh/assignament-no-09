96c1d5908b7fb3ede1e5ffbce06dae19ec89b807
hizarh <https://github.com/hizarh/practic1122.git>
2024-07-06 21:12:16 -0700

    first project

---
+++ scritp1.js
@@ -0,0 +1,24 @@
+// const arr = [];
+// // const result= '';
+// for(let i=0; i<10; i++){
+//   arr[i]+prompt("enter a number")
+
+//     // result += ${num}* ${i} = ${num*i}/n;
+//  console.log('${arr} * ${i} = $ {arr * i}');
+
+// }
+
+// var arr = [];
+// var fact='';
+// for(let i=0; i<5; i++){
+//   arr[i]=+prompt('enter a number');
+//     fact=fact * i;
+//     // console.log(fact[i]);
+//   console.log(fact * arr[i]);
+// }
+
+
+
+const str =  prompt("enter a string");
+const reversestr=str.split("").reverse().join('');
+console.log('reverse of the string : ${reversestr}');
